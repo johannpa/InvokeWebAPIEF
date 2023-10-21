169a2a6cb3f9f83916236642f292df9170fbe922
johannpa <johann_paque@hotmail.fr>
2023-10-22 01:24:35 +0200

    Create controller CryptoesController and Data with scaffolding auto generate

---
+++ InvokeWebAPIEF/Data/InvokeWebAPIEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InvokeWebAPIEF.Models;
+
+namespace InvokeWebAPIEF.Data
+{
+    public class InvokeWebAPIEFContext : DbContext
+    {
+        public InvokeWebAPIEFContext (DbContextOptions<InvokeWebAPIEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InvokeWebAPIEF.Models.Crypto> Crypto { get; set; } = default!;
+    }
+}
--- InvokeWebAPIEF/Program.cs
+++ InvokeWebAPIEF/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using InvokeWebAPIEF.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<InvokeWebAPIEFContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InvokeWebAPIEFContext") ?? throw new InvalidOperationException("Connection string 'InvokeWebAPIEFContext' not found.")));
 
 // Add services to the container.
 
