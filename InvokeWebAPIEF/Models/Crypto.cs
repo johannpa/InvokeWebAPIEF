99a7b63456fa56b7422c079a4a854edd6187daa9
johannpa <johann_paque@hotmail.fr>
2023-10-22 00:51:19 +0200

    Create Models folder with a model in it Crypto and add Entity design's nuget

---
+++ InvokeWebAPIEF/Models/Crypto.cs
@@ -0,0 +1,9 @@
+﻿namespace InvokeWebAPIEF.Models
+{
+    public class Crypto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+    }
+}
